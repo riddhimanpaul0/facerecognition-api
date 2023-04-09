4bb1ac840b283d57c65cb3cd816ca62f92f22ebf
riddhimanpaul0 <riddhimanpaul0@gmail.com>
2023-04-09 11:11:46 -0400

    dependencies

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.put('/image', (req, res) => {
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+    // console.log(`app is running on port 3000`);
     console.log(`app is running on port ${process.env.PORT}`);
     
 })
