a004941c6559308c3df0d88f8e3436d2a9635894
riddhimanpaul0 <riddhimanpaul0@gmail.com>
2023-04-09 19:13:03 -0400

    for render

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
 
-const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
-  });
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//       connectionString : process.env.DATABASE_URL,
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//     }
+//   });
 
 
 const app = express();
