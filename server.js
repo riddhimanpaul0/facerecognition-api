823b82e1fdf1bf8eefd6da72fb8e2d4b2767dbbc
riddhimanpaul0 <riddhimanpaul0@gmail.com>
2023-04-09 17:59:23 -0400

    fix2

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post('/register', (req, res) => {
             .then(trx.commit)
             .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .catch(err => res.status(400).json('unable to register2'))
     
 })
 
